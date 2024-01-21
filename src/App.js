c21b123be2206dffddd3b41ebe7b1dfce37fb0da
sohee <sohee5143@naver.com>
2024-01-22 01:50:49 +0900

    ✨ Feat: 상세페이지 상단 ui 제작

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./pages/Main";
 import MyPage from "./pages/Mypage";
 import Search from "./pages/Search";
 import FlowWrite from "./pages/FlowWrite"; // 플로우 만들기 페이지
-import RecreationDetail from "./pages/RecreationDetail"; // 레크레이션 상세정보 페이지
+import RecreationDetail from "./pages/recreation/RecreationDetail"; // 레크레이션 상세정보 페이지
 import FlowMy from "./pages/FlowMy"; // 내 일정 플로우 페이지
 
 function App() {
   return (
     <div className="App">
-       <Header />
-        <Routes>
-          <Route path="/" element={<Main />} /> {/* 메인 */}
-
-          <Route path="/mypage" element={<MyPage />} /> {/* 마이 페이지 */}
-
-          <Route path="/search" element={<Search />} /> {/* 검색 페이지 */}
-          <Route path="/flow/write" element={<FlowWrite />} /> {/* 플로우 만들기 페이지 */}
-
-          <Route path="/flow/my" element={<FlowMy />} /> {/* 내 일정 플로우 페이지 */}
-
-          <Route path="/recreation/detail" element={<RecreationDetail />} /> {/* 레크레이션 상세정보 페이지 */}
-
-        </Routes>
-        <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} /> {/* 메인 */}
+        <Route path="/mypage" element={<MyPage />} /> {/* 마이 페이지 */}
+        <Route path="/search" element={<Search />} /> {/* 검색 페이지 */}
+        <Route path="/flow/write" element={<FlowWrite />} />{" "}
+        {/* 플로우 만들기 페이지 */}
+        <Route path="/flow/my" element={<FlowMy />} />{" "}
+        {/* 내 일정 플로우 페이지 */}
+        <Route path="/recreation/detail" element={<RecreationDetail />} />{" "}
+        {/* 레크레이션 상세정보 페이지 */}
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/pages/RecreationDetail.jsx
+++
@@ -1,6 +0,0 @@
-export default function RecreationDetail() {
-
-    return (
-        <div>레크레이션 상세 페이지</div> // 코드 작성 시 삭제해주세요!
-    )
-}
