3329787ccdd58f386d31c98db62e4f8e9160c962
yongmin01 <heart77123@naver.com>
2024-02-20 01:36:09 +0900

    ✨ Feat: 로그인 안되어 있는 경우 로그인 유도 alert창 띄우기

--- src/apis/user.js
+++ src/apis/user.js
@@ -9,8 +9,15 @@ export const privateAPI = axios.create({
 
 // 리프레시 토큰 요청 함수
 export async function postRefreshToken() {
-  const response = await publicAPI.post("/api/auth/refresh");
-  return response;
+  try {
+    const response = await publicAPI.post("/api/auth/refresh");
+    if (response.status === 401) {
+      console.log("aaaa");
+      return response;
+    }
+  } catch (error) {
+    console.log("dfdddd : ", error);
+  }
 }
 
 // 로그인 여부 반환 함수
@@ -60,12 +67,14 @@ privateAPI.interceptors.response.use(
           originRequest.headers.Authorization = `Bearer ${newAccessToken}`;
           return axios(originRequest);
           //리프레시 토큰 요청이 실패할때(리프레시 토큰도 만료되었을때 = 재로그인 안내)
-        } else if (response.status === 404) {
+        } else if (response.status === 401) {
           alert("세션이 만료되었습니다. 재로그인 해주세요.");
         } else {
           console.log("인터셉터 내부 기타 에러 : ", error);
         }
       } else {
+        // 리프레시 토큰이 없는 경우 = 로그인 안내
+        alert("로그인이 필요한 서비스입니다.");
         return "need login";
       }
     }
