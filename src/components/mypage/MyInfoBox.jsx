c5c64a05efa4e07663a0cf52ffa5a4399a26c38f
yongmin01 <heart77123@naver.com>
2024-09-28 15:06:46 +0900

    🐛Fix: 마이페이지 닉네임 글자수 버그 수정 및 새로고침 시 데이터 리셋 버그 수정

--- src/components/mypage/MyInfoBox.jsx
+++ src/components/mypage/MyInfoBox.jsx
@@ -5,13 +5,31 @@ import NicknameChangeModal from "../modal/NicknameChangeModal";
 import LogoutP from "../../assets/mypage/LogoutImg.svg";
 import WarnLogo from "../../assets/mypage/WarnLogo.svg";
 import { privateAPI } from "../../apis/user";
+import LoadingSpinner from "../LoadingSpinner";
 
-export default function MyInfoBox({ content }) {
+export default function MyInfoBox() {
   const [isGoOutModalOpen, setGoOutModalOpen] = useState(false);
   const [isNicknameChangeModalOpen, setIsNicknameChangeModal] = useState(false);
   const [nickname, setNickname] = useState("");
+  const [previousNickname, setPreviousNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  localStorage.setItem("userimage", content.profileImage);
+  useEffect(() => {
+    const call = async () => {
+      setLoading(true);
+      try {
+        const response = await privateAPI.get(`/api/users/me`);
+        setPreviousNickname(response.data.result.username);
+        localStorage.setItem("userimage", response.data.result.profileImage);
+        setEmail(response.data.result.email);
+        setLoading(false);
+      } catch (error) {
+        console.log("내 정보 로드 요청 에러 : ", error);
+      }
+    };
+    call();
+  }, []);
 
   const openGoOututModal = () => {
     setGoOutModalOpen(true);
@@ -21,8 +39,10 @@ export default function MyInfoBox({ content }) {
     setGoOutModalOpen(false);
   };
 
-  const handleNickname = (e) => {
-    setNickname(e.target.value);
+  const handleNickname = (e, maxlength) => {
+    if (e.target.value.length > maxlength)
+      setNickname(e.target.value.substr(0, maxlength));
+    else setNickname(e.target.value);
   };
 
   const ChangeName = async () => {
@@ -38,39 +58,55 @@ export default function MyInfoBox({ content }) {
     }
   };
 
-  useEffect(() => {
-    setNickname(content.username);
-  }, [content.username]);
-
   return (
     <MyInfo>
-      <MyTitle>카카오 계정</MyTitle>
-      <MyInput value={content.email} readOnly />
-      <MyTitle2>닉네임</MyTitle2>
-      <MyInput value={nickname} maxLength={10} onChange={handleNickname} />
-      <WarnSpace>
-        <WarnImg src={WarnLogo} />
-        <Warn>닉네임은 공백포함 10자까지 작성 가능합니다.</Warn>
-      </WarnSpace>
-      <ButtonSection>
-        <OutBut onClick={openGoOututModal}>회원탈퇴</OutBut>
-        <SaveBut onClick={ChangeName}>저장하기</SaveBut>
-      </ButtonSection>
-      {isGoOutModalOpen && (
-        <LogoutModal>
-          <ModalContent>
-            <ModalTitle>회원탈퇴 하시게요?</ModalTitle>
-            <SemiTitle>한번 탈퇴하면 되돌릴 수 없습니다.</SemiTitle>
-            <LogoutImg src={LogoutP} />
-            <ModalBut>
-              <LogoutButton onClick={closeGoOutModal}>회원탈퇴</LogoutButton>
-              <CloseButton onClick={closeGoOutModal}>닫기</CloseButton>
-            </ModalBut>
-          </ModalContent>
-        </LogoutModal>
-      )}
-      {isNicknameChangeModalOpen && (
-        <NicknameChangeModal handleModal={setIsNicknameChangeModal} />
+      {loading ? (
+        <LoadingSpinner
+          comment={
+            <span>
+              정보를 불러오는 중입니다.
+              <br />
+              잠시만 기다려주세요.
+            </span>
+          }
+        />
+      ) : (
+        <>
+          <MyTitle>카카오 계정</MyTitle>
+          <MyInput value={email} readOnly className="account" />
+          <MyTitle2>닉네임</MyTitle2>
+          <MyInput
+            value={nickname}
+            onChange={(e) => handleNickname(e, 10)}
+            placeholder={previousNickname}
+          />
+          <WarnSpace>
+            <WarnImg src={WarnLogo} />
+            <Warn>닉네임은 공백포함 10자까지 작성 가능합니다.</Warn>
+          </WarnSpace>
+          <ButtonSection>
+            <OutBut onClick={openGoOututModal}>회원탈퇴</OutBut>
+            <SaveBut onClick={ChangeName}>저장하기</SaveBut>
+          </ButtonSection>
+          {isGoOutModalOpen && (
+            <LogoutModal>
+              <ModalContent>
+                <ModalTitle>회원탈퇴 하시게요?</ModalTitle>
+                <SemiTitle>한번 탈퇴하면 되돌릴 수 없습니다.</SemiTitle>
+                <LogoutImg src={LogoutP} />
+                <ModalBut>
+                  <LogoutButton onClick={closeGoOutModal}>
+                    회원탈퇴
+                  </LogoutButton>
+                  <CloseButton onClick={closeGoOutModal}>닫기</CloseButton>
+                </ModalBut>
+              </ModalContent>
+            </LogoutModal>
+          )}
+          {isNicknameChangeModalOpen && (
+            <NicknameChangeModal handleModal={setIsNicknameChangeModal} />
+          )}
+        </>
       )}
     </MyInfo>
   );
@@ -78,7 +114,6 @@ export default function MyInfoBox({ content }) {
 
 const MyInfo = styled.div`
   width: 687px;
-  height: 434px;
 `;
 
 const MyTitle = styled.div`
@@ -97,9 +132,16 @@ const MyInput = styled.input`
   height: 70px;
   border-radius: 20px;
   border: solid #cacdd2 1px;
-  color: #cacdd2;
+  color: #1b1d1f;
   font-size: 20px;
   padding-left: 25px;
+
+  &.account {
+    color: #cacdd2;
+  }
+  &::placeholder {
+    color: #cacdd2;
+  }
 `;
 
 const WarnSpace = styled.div`
