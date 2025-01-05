afbb1bd297348818f49461f90c7a679bb7a0f707
yongmin01 <heart77123@naver.com>
2025-01-05 15:31:04 +0900

    💄 Design: 목적, 키워드, 해시태그 칩

---
+++ src/components/chip/PurposeChip.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function PurposeChip({ text }) {
+  return <Chip>{text}</Chip>;
+}
+
+const Chip = styled.div`
+  width: fit-content;
+  height: 3.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0.8em 1.69em;
+  border-radius: 3.125rem;
+  background-color: ${({ theme }) => theme.color.main03};
+  color: ${({ theme }) => theme.color.grayscale01};
+  ${({ theme }) => theme.text.button}
+`;
