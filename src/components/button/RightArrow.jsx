9c9b242c03378fddfe5782a7759851fea27bb77a
eomgerm <fishbread00@gmail.com>
2025-01-06 15:50:33 +0900

    :sparkles: Feat: 공통 화살표 버튼 컴포넌트

---
+++ src/components/button/RightArrow.jsx
@@ -0,0 +1,20 @@
+import rightArrowImage from '../../assets/main/nextArrowIcon.svg';
+import styled from 'styled-components';
+
+export default function RightArrow() {
+  return (
+    <ArrowButton>
+      <img src={rightArrowImage} alt="다음" />
+    </ArrowButton>
+  );
+}
+
+const ArrowButton = styled.button`
+  width: 2.6rem;
+  height: 2.6rem;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 9999px;
+  padding-top: 5px;
+  filter: drop-shadow(0px 5px 10px rgba(27, 29, 31, 0.15));
+`;
