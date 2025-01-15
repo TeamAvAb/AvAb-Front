7de496c21fac6292ec24d5dc1f9039f1b693d842
eomgerm <fishbread00@gmail.com>
2025-01-16 00:10:35 +0900

    :recycle: Refactor: 버튼 스타일 수정

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -25,7 +25,7 @@ const StyledButton = styled.button`
   background-color: ${({ backgroundColor, theme }) => theme.color[backgroundColor]};
   border-radius: 9999px;
   color: ${({ color, theme }) => theme.color[color]};
-  padding: ${({ size }) => (size === 'small' ? '0.5rem 2rem' : '1rem 2rem')};
+  padding: ${({ size }) => (size === 'small' ? '0.5rem 1.5rem' : '1rem 2rem')};
   font-size: ${({ theme }) => theme.text.button.fontSize};
   font-weight: ${({ theme }) => theme.text.button.fontWeight};
   border: ${({ border }) => (border ? '1px solid black' : 'none')};
