7757fb6623c97a5bc2f3dfc8b7efe6a1f72268d0
yongmin01 <heart77123@naver.com>
2025-01-26 02:42:38 +0900

    ♻️Refactor: 커스텀 패딩 props 추가

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
-export default function Button({ children, onClick, backgroundColor, color, border }) {
+export default function Button({ children, onClick, backgroundColor, color, border, padding }) {
   return (
-    <StyledButton backgroundColor={backgroundColor} color={color} border={border} onClick={onClick}>
+    <StyledButton
+      backgroundColor={backgroundColor}
+      color={color}
+      border={border}
+      padding={padding}
+      onClick={onClick}
+    >
       {children}
     </StyledButton>
   );
@@ -12,7 +18,7 @@ const StyledButton = styled.button`
   background-color: ${({ backgroundColor, theme }) => theme.color[backgroundColor]};
   border-radius: 9999px;
   color: ${({ color, theme }) => theme.color[color]};
-  padding: 1rem 2rem;
+  padding: ${({ padding }) => (padding ? padding : '1rem 2rem')};
   font-size: ${({ theme }) => theme.text.button.fontSize};
   font-weight: ${({ theme }) => theme.text.button.fontWeight};
   border: ${({ border }) => (border ? '1px solid black' : 'none')};
