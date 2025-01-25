dbfda5021dd6bd30c13980681e0182f9b5d57fc2
yongmin01 <heart77123@naver.com>
2025-01-26 02:59:45 +0900

    ♻️Refactor: 일정플로우 탭 로직 교체 및 css 통일

--- src/pages/MyFlow.jsx
+++ src/pages/MyFlow.jsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { publicAPI, privateAPI } from '../apis/user';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import Pagination from '../components/pagination/Pagination';
+import LoadingSpinner from '../components/LoadingSpinner.jsx';
+import FlowCardD from '../components/card/flowCard/FlowCardD';
+import Button from '../components/button/Button';
+import useModal from '../hooks/useModal';
+import FlowDeleteModal from '../components/modal/FlowDeleteModal';
+import useLoginStore from '../stores/loginStore';
+import useLoginModalStore from '../stores/loginModalStore';
 import penguinImg from '../assets/myflow/penguin.png';
 import noFlowImg from '../assets/myflow/noFlow.png';
-import MadeFlowBox from '../components/flow/MadeFlowBox';
-import { useNavigate } from 'react-router-dom';
-import Pagination from '../components/pagination/Pagination';
-import useLoginModalStore from '../stores/loginModalStore';
-import useLoginStore from '../stores/loginStore';
 
 export default function MyFlow() {
   const { modalControl } = useLoginModalStore();
   const { isLoggedIn } = useLoginStore((state) => state);
+  const { ModalWrapper, openModal, closeModal } = useModal();
 
   const navigate = useNavigate();
   const moveToWatch = () => {
@@ -35,197 +40,218 @@ export default function MyFlow() {
   const [currentPage, setCurrentPage] = useState(0);
   // 전체 페이지 수
   const [pages, setPages] = useState(1);
-  // 삭제 시 렌더링을 위한 상태
-  const [doDel, setDoDel] = useState(false);
 
-  // 처음 렌더링 시에만 데이터 불러오기
+  const fetchData = async () => {
+    setLoading(true);
+    if (isLoggedIn) {
+      const response = await privateAPI.get(`/api/users/me/flows?page=${currentPage}`);
+      setDatas(response.data.result.flowList);
+      setPages(response.data.result.totalPages);
+    } else {
+      const response = await publicAPI.get(`/api/users/me/flows?page=${currentPage}`);
+      setDatas(response.data.result.flowList);
+      setPages(response.data.result.totalPages);
+    }
+    setLoading(false);
+  };
+
+  const handleDeleteClick = (flowId) => {
+    openModal();
+    localStorage.setItem('flowDeleteTarget', flowId);
+  };
+
+  // 초기 렌더링 및 페이지 변경에 따른 데이터 페칭
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      if (isLoggedIn) {
-        const response = await privateAPI.get(`/api/users/me/flows?page=${currentPage}`);
-        console.log('전체 레크:', response);
-        setDatas(response.data.result.flowList);
-        setPages(response.data.result.totalPages);
-      } else {
-        const response = await publicAPI.get(`/api/users/me/flows?page=${currentPage}`);
-        setDatas(response.data.result.flowList);
-        setPages(response.data.result.totalPages);
-      }
-      setLoading(false);
-    };
     fetchData();
-    setDoDel(false);
-  }, [currentPage, doDel]);
-
-  useEffect(() => {
-    console.log(datas);
-  }, [datas]);
+  }, [currentPage]);
 
   return (
-    <MyFlowWrap>
+    <Page>
       {/* 플로우 왼쪽 메뉴바 */}
-      <MyFlowMenuContainer>
-        <MyFlowMenuTitle>일정플로우</MyFlowMenuTitle>
-        <MyFlowMenuBox onClick={moveToWatch}>플로우 구경하기</MyFlowMenuBox>
-        <MyFlowMenuBox style={{ backgroundColor: '#B1BEFF' }}>내가 만든 일정플로우</MyFlowMenuBox>
-        <MyFlowMenuBox onClick={moveToScrap}>스크랩 일정 플로우</MyFlowMenuBox>
-      </MyFlowMenuContainer>
+      <SubNavbar>
+        <NavbarTitle>일정플로우</NavbarTitle>
+        <NavbarOption onClick={moveToWatch}>플로우 구경하기</NavbarOption>
+        <NavbarOption className="current">내가 만든 일정플로우</NavbarOption>
+        <NavbarOption onClick={moveToScrap}>스크랩 일정 플로우</NavbarOption>
+      </SubNavbar>
 
-      <MyFlowContainer>
-        <div>
-          {/* 내가 만든 일정플로우 - Title */}
-          <MyFlowBoxContainer>
-            <MyFlowBoxImage src={penguinImg} />
-            <TitleBox>
-              <MyFlowBoxTitle onClick={moveToMakeFlow}>일정플로우 만들기</MyFlowBoxTitle>
-            </TitleBox>
-          </MyFlowBoxContainer>
-          {/* 내가 만든 일정플로우 - Grid */}
-          {datas.length !== 0 ? (
-            <div>
-              <MyFlowBoxParent>
-                {datas && <MadeFlowBox datas={datas} setDoDel={setDoDel} />}
-              </MyFlowBoxParent>
-              {/* 페이지번호 */}
-              <Pagination
-                currentPage={currentPage}
-                pageNum={pages}
-                setCurrentPage={setCurrentPage}
-              />
-            </div>
-          ) : (
-            <MyFlowNoneBox>
-              <MyFlowNoneImg src={noFlowImg} />
-              <MyFlowNoneDetail>
-                <div style={{ fontSize: '24px', fontWeight: 'bold' }}>
-                  내가 만든 일정플로우가 없습니다!
-                </div>
-                <div style={{ fontSize: '20px', marginTop: '8px' }}>
-                  위의 버튼을 눌러 나만의 일정플로우를 만들어 보세요.
-                </div>
-              </MyFlowNoneDetail>
-            </MyFlowNoneBox>
-          )}
-        </div>
-      </MyFlowContainer>
-      <RightSide />
-    </MyFlowWrap>
+      <MainSection>
+        {/* 내가 만든 일정플로우 - Title */}
+        <FlowMakeBox>
+          <FlowMakeCharacter src={penguinImg} />
+          <FlowMakeBtn onClick={moveToMakeFlow}>일정플로우 만들기</FlowMakeBtn>
+        </FlowMakeBox>
+        {loading ? (
+          <LoadingSpinner
+            comment={
+              <span>
+                데이터를 불러오는 중입니다.
+                <br />
+                잠시만 기다려주세요.
+              </span>
+            }
+          />
+        ) : datas && datas.length > 0 ? (
+          <>
+            <FlowBox>
+              {datas.map((data) => (
+                <FlowCardD content={data} ismine="true">
+                  <>
+                    <Button backgroundColor="grayscale01" color="main05" padding="0.5rem 1.25rem">
+                      수정
+                    </Button>
+                    <Button
+                      backgroundColor="main05"
+                      color="grayscale04"
+                      border="border"
+                      padding="0.5rem 1.25rem"
+                      onClick={() => handleDeleteClick(data.id)}
+                    >
+                      삭제
+                    </Button>
+                  </>
+                </FlowCardD>
+              ))}
+              <ModalWrapper children={<FlowDeleteModal close={closeModal} refetch={fetchData} />} />
+            </FlowBox>
+            <Pagination currentPage={currentPage} pageNum={pages} setCurrentPage={setCurrentPage} />
+          </>
+        ) : (
+          <NoneAlertBox>
+            <NoneAlertImg src={noFlowImg} />
+            <NoneAlertTextBox>
+              <NoneAlertText className="title"> 내가 만든 일정플로우가 없습니다!</NoneAlertText>
+              <NoneAlertText>위의 버튼을 눌러 나만의 일정플로우를 만들어 보세요.</NoneAlertText>
+            </NoneAlertTextBox>
+          </NoneAlertBox>
+        )}
+      </MainSection>
+      <PageMarginRight />
+    </Page>
   );
 }
 
-const MyFlowWrap = styled.div`
+const Page = styled.div`
   display: flex;
-  background-color: #f7f8f9;
 `;
 
-const RightSide = styled.div`
-  width: 5.7325%;
-  background-color: #f7f8f9;
-`;
-
-// 일정플로우-구경하기 왼쪽메뉴바 > ~ MyFlowMenuBox
-const MyFlowMenuContainer = styled.div`
-  background-color: white;
-  width: 320px;
-  height: 713px;
+// 왼쪽 네비게이션바
+const SubNavbar = styled.aside`
+  width: 20rem;
   box-sizing: border-box;
-  font-size: 22px;
-  border: solid #cacdd2 1px;
+  border: 0.5px solid ${({ theme }) => theme.color.grayscale05};
   border-bottom: none;
-  border-right: 0.5px solid #cacdd2;
-  color: #1b1d1f;
+  background-color: ${({ theme }) => theme.color.main05};
+  font-size: ${({ theme }) => theme.text.nav.fontSize};
+  font-weight: ${({ theme }) => theme.text.nav.fontWeight};
+  color: ${({ theme }) => theme.color.grayscale01};
 `;
-
-const MyFlowMenuTitle = styled.div`
-  width: 260px;
-  padding: 30px;
-  font-size: 22px;
-  border-bottom: solid #cacdd2 1px;
+const NavbarTitle = styled.div`
+  height: 5.56rem;
+  box-sizing: border-box;
+  padding: 1.87rem;
+  border-bottom: 1px solid ${({ theme }) => theme.color.grayscale05};
 `;
-
-const MyFlowMenuBox = styled.div`
-  text-align: center;
+const NavbarOption = styled.div`
+  width: 20rem;
+  height: 4.18rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 0.5px solid ${({ theme }) => theme.color.grayscale05};
+  box-sizing: border-box;
   cursor: pointer;
-  padding: 20px;
-  width: 280px;
-  border-bottom: solid #cacdd2 1px;
+
+  &.current {
+    background-color: ${({ theme }) => theme.color.secondary04};
+    font-weight: 700;
+  }
 `;
 
-const MyFlowContainer = styled.div`
-  background-color: white;
+const MainSection = styled.main`
   display: flex;
   flex: 1;
   flex-direction: column;
   align-items: center;
-  border-right: 0.5px solid #cacdd2;
+  padding: 1.88rem 8.38rem 7rem;
+  border-right: 0.5px solid ${({ theme }) => theme.color.grayscale05};
 `;
 
-// 내가 만든 일정 플로우 - Title
-const MyFlowBoxContainer = styled.div`
+// 플로우 만들기 (캐릭터 + 버튼)
+const FlowMakeBox = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
-  width: 100%;
-  margin-top: 30px;
+  margin-right: auto;
 `;
-
-const MyFlowBoxImage = styled.img`
-  width: 199.65px;
-  height: 199.65px;
-  position: relative;
+const FlowMakeCharacter = styled.img`
+  width: 12.47rem;
   z-index: 2;
 `;
-
-const TitleBox = styled.div`
-  background-color: #19297c;
-  border-radius: 15vh;
-  width: 527px;
-  height: 109px;
+const FlowMakeBtn = styled.button`
+  position: absolute;
+  left: 10.94rem;
+  width: 32.93rem;
+  height: 6.81rem;
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 1;
-  position: relative;
-  left: -23.65px;
+  border-radius: 6.25rem;
+  background-color: ${({ theme }) => theme.color.main01};
+  font-size: ${({ theme }) => theme.text.h2.fontSize};
+  font-weight: ${({ theme }) => theme.text.h2.fontWeight};
+  color: ${({ theme }) => theme.color.main05};
   cursor: pointer;
-
   &:hover {
-    background-color: #4036ed;
+    background-color: ${({ theme }) => theme.color.main02};
     transition: 0.2s;
   }
 `;
 
-const MyFlowBoxTitle = styled.div`
-  color: white;
-  width: 343px;
-  height: 57px;
-  font-weight: bold;
-  font-size: 47px;
-`;
-
 // 플로우 박스 - Grid
-const MyFlowBoxParent = styled.div`
+const FlowBox = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 370px);
-  row-gap: 20px;
-  column-gap: 120px;
-  margin-top: 39px;
+  grid-template-columns: repeat(2, 1fr);
+  row-gap: 1.25rem;
+  column-gap: 7.5rem;
+  margin: 2.44rem 0 5.08rem;
 `;
 
-// 내가 만든 일정 플로우 - None
-const MyFlowNoneBox = styled.div`
+// 일정플로우 없는 경우
+const NoneAlertBox = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2.5rem;
   text-align: center;
-  margin-bottom: 237px;
 `;
-
-const MyFlowNoneImg = styled.img`
-  width: 120px;
-  height: 120px;
+const NoneAlertImg = styled.img`
+  width: 7.5rem;
 `;
-
-const MyFlowNoneDetail = styled.div`
+const NoneAlertTextBox = styled.div`
   width: 100%;
-  margin-top: 40px;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+const NoneAlertText = styled.span`
+  font-size: ${({ theme }) => theme.text.paragraph.fontSize};
+  font-weight: ${({ theme }) => theme.text.paragraph.fontWeight};
+
+  &.title {
+    font-size: ${({ theme }) => theme.text.h4.fontSize};
+    font-weight: ${({ theme }) => theme.text.h4.fontWeight};
+  }
+`;
+
+// 오른쪽 여백
+const PageMarginRight = styled.div`
+  width: 5.7325%;
+  background-color: #f7f8f9;
+`;
+
+const CustomButton = styled(Button)`
+  padding: 0.5rem 1.25rem;
 `;
