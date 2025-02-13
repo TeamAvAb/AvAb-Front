061719f49c9a17f91375ab97c40ef112bc99590c
yongmin01 <heart77123@naver.com>
2025-02-13 22:03:19 +0900

    ♻️Refactor: 플로우탭 레이아웃 적용

--- src/pages/MyFlow.jsx
+++ src/pages/MyFlow.jsx
@@ -1,33 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { privateAPI, publicAPI } from '../apis/user';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import Pagination from '../components/pagination/Pagination';
 import useModal from '../hooks/useModal';
 import FlowDeleteModal from '../components/modal/FlowDeleteModal';
 import useLoginStore from '../stores/loginStore';
-import useLoginModalStore from '../stores/loginModalStore';
-import penguinImg from '../assets/myflow/penguin.png';
 import noFlowImg from '../assets/myflow/noFlow.png';
-import LoadingSpinner from '../components/common/LoadingSpinner';
 import Button from '../components/common/button/Button';
 import FlowCardD from '../components/common/card/flowCard/FlowCardD';
-import SideNavLayout from '../layout/SideNavLayout';
+import FlowTabLayout from '../components/flow/FlowTabLayout';
 
 export default function MyFlow() {
-  const { modalControl } = useLoginModalStore();
   const { isLoggedIn } = useLoginStore((state) => state);
   const { ModalWrapper, openModal, closeModal } = useModal();
-
-  const navigate = useNavigate();
-
-  const moveToMakeFlow = () => {
-    if (isLoggedIn) {
-      navigate(`/flow/write`);
-    } else {
-      modalControl();
-    }
-  };
 
   // 데이터 가져오기
   const [datas, setDatas] = useState([]);
@@ -36,18 +20,18 @@ export default function MyFlow() {
   // 현재 페이지 상태
   const [currentPage, setCurrentPage] = useState(0);
   // 전체 페이지 수
-  const [pages, setPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   const fetchData = async () => {
     setLoading(true);
     if (isLoggedIn) {
       const response = await privateAPI.get(`/api/users/me/flows?page=${currentPage}`);
       setDatas(response.data.result.flowList);
-      setPages(response.data.result.totalPages);
+      setTotalPages(response.data.result.totalPages);
     } else {
       const response = await publicAPI.get(`/api/users/me/flows?page=${currentPage}`);
       setDatas(response.data.result.flowList);
-      setPages(response.data.result.totalPages);
+      setTotalPages(response.data.result.totalPages);
     }
     setLoading(false);
   };
@@ -63,95 +47,49 @@ export default function MyFlow() {
   }, [currentPage]);
 
   return (
-    <SideNavLayout selectedPage="my-flow" parentTab="flow">
-      <MainSection>
-        {/* 내가 만든 일정플로우 - Title */}
-        <FlowMakeBox>
-          <FlowMakeCharacter src={penguinImg} />
-          <FlowMakeBtn onClick={moveToMakeFlow}>일정플로우 만들기</FlowMakeBtn>
-        </FlowMakeBox>
-        {loading ? (
-          <LoadingSpinner />
-        ) : datas && datas.length > 0 ? (
-          <>
-            <FlowBox>
-              {datas.map((data) => (
-                <FlowCardD content={data} isOwner="true">
-                  <>
-                    <Button backgroundColor="grayscale01" color="main05" border size="xs">
-                      수정
-                    </Button>
-                    <Button
-                      onClick={() => handleDeleteClick(data.id)}
-                      backgroundColor="main05"
-                      color="grayscale04"
-                      border
-                      size="xs"
-                    >
-                      삭제
-                    </Button>
-                  </>
-                </FlowCardD>
-              ))}
-              <ModalWrapper children={<FlowDeleteModal close={closeModal} refetch={fetchData} />} />
-            </FlowBox>
-            <Pagination currentPage={currentPage} pageNum={pages} setCurrentPage={setCurrentPage} />
-          </>
-        ) : (
-          <NoneAlertBox>
-            <NoneAlertImg src={noFlowImg} />
-            <NoneAlertTextBox>
-              <NoneAlertText className="title"> 내가 만든 일정플로우가 없습니다!</NoneAlertText>
-              <NoneAlertText>위의 버튼을 눌러 나만의 일정플로우를 만들어 보세요.</NoneAlertText>
-            </NoneAlertTextBox>
-          </NoneAlertBox>
-        )}
-      </MainSection>
-    </SideNavLayout>
+    <FlowTabLayout
+      selectedPage="my-flow"
+      isloading={loading}
+      currentPage={currentPage}
+      totalPages={totalPages}
+      handlePaginationClick={setCurrentPage}
+    >
+      {!loading && datas && datas.length > 0 ? (
+        <>
+          <FlowBox>
+            {datas.map((data) => (
+              <FlowCardD content={data} isOwner="true">
+                <>
+                  <Button backgroundColor="grayscale01" color="main05" border size="xs">
+                    수정
+                  </Button>
+                  <Button
+                    onClick={() => handleDeleteClick(data.id)}
+                    backgroundColor="main05"
+                    color="grayscale04"
+                    border
+                    size="xs"
+                  >
+                    삭제
+                  </Button>
+                </>
+              </FlowCardD>
+            ))}
+            <ModalWrapper children={<FlowDeleteModal close={closeModal} refetch={fetchData} />} />
+          </FlowBox>
+        </>
+      ) : (
+        <NoneAlertBox>
+          <NoneAlertImg src={noFlowImg} />
+          <NoneAlertTextBox>
+            <NoneAlertText className="title"> 내가 만든 일정플로우가 없습니다!</NoneAlertText>
+            <NoneAlertText>위의 버튼을 눌러 나만의 일정플로우를 만들어 보세요.</NoneAlertText>
+          </NoneAlertTextBox>
+        </NoneAlertBox>
+      )}
+    </FlowTabLayout>
   );
 }
-
-const MainSection = styled.main`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  align-items: center;
-  padding: 1.88rem 8.38rem 7rem;
-  border-right: 0.5px solid ${({ theme }) => theme.color.grayscale05};
-`;
-
-// 플로우 만들기 (캐릭터 + 버튼)
-const FlowMakeBox = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  margin-right: auto;
-`;
-const FlowMakeCharacter = styled.img`
-  width: 12.47rem;
-  z-index: 2;
-`;
-const FlowMakeBtn = styled.button`
-  position: absolute;
-  left: 10.94rem;
-  width: 32.93rem;
-  height: 6.81rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1;
-  border-radius: 6.25rem;
-  background-color: ${({ theme }) => theme.color.main01};
-  font-size: ${({ theme }) => theme.text.h2.fontSize};
-  font-weight: ${({ theme }) => theme.text.h2.fontWeight};
-  color: ${({ theme }) => theme.color.main05};
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.color.main02};
-    transition: 0.2s;
-  }
-`;
 
 // 플로우 박스 - Grid
 const FlowBox = styled.div`
@@ -188,14 +126,4 @@ const NoneAlertText = styled.span`
     font-size: ${({ theme }) => theme.text.h4.fontSize};
     font-weight: ${({ theme }) => theme.text.h4.fontWeight};
   }
-`;
-
-// 오른쪽 여백
-const PageMarginRight = styled.div`
-  width: 5.7325%;
-  background-color: #f7f8f9;
-`;
-
-const CustomButton = styled(Button)`
-  padding: 0.5rem 1.25rem;
 `;
--- src/pages/WatchFlow.jsx
+++ src/pages/WatchFlow.jsx
@@ -1,31 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { privateAPI, publicAPI } from '../apis/user.js';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import Pagination from '../components/pagination/Pagination.jsx';
 import useLoginModalStore from '../stores/loginModalStore.js';
 import noScrapImg from '../assets/scrapflow/noScrap.png';
-import penguinImg from '../assets/watchflow/penguin.png';
 import useLoginStore from '../stores/loginStore.js';
 import SortControl from '../components/common/SortControl';
-import LoadingSpinner from '../components/common/LoadingSpinner';
 import ScrapBtn from '../components/common/button/ScrapBtn';
 import FlowCardD from '../components/common/card/flowCard/FlowCardD';
-import SideNavLayout from '../layout/SideNavLayout.jsx';
+import FlowTabLayout from '../components/flow/FlowTabLayout.jsx';
 
 export default function WatchFlow() {
   const { modalControl } = useLoginModalStore();
   const { isLoggedIn } = useLoginStore((state) => state);
-
-  const navigate = useNavigate();
-
-  const moveToMakeFlow = () => {
-    if (isLoggedIn) {
-      navigate(`/flow/write`);
-    } else {
-      modalControl();
-    }
-  };
 
   // 데이터 가져오기
   const [datas, setDatas] = useState([]);
@@ -98,89 +84,40 @@ export default function WatchFlow() {
   }, [currentPage, order]);
 
   return (
-    <SideNavLayout selectedPage="watch-flow" parentTab="flow">
-      <MainSection>
-        <FlowMakeBox>
-          <FlowMakeCharacter src={penguinImg} />
-          <FlowMakeBtn onClick={moveToMakeFlow}>일정플로우 만들기</FlowMakeBtn>
-        </FlowMakeBox>
-        <SortControl setOption={setOrder} selectedOption={order} marginright="24px" isFlow={true} />
-        {loading ? (
-          <LoadingSpinner />
-        ) : datas && datas.length > 0 ? (
-          <>
-            <FlowBox>
-              {datas.map((data) => (
-                <FlowCardD content={data}>
-                  <ScrapBtn
-                    flowId={data.id}
-                    isScrap={data.isScraped}
-                    onClick={() => handleScrapBtnClick(data.id)}
-                  />
-                </FlowCardD>
-              ))}
-            </FlowBox>
-            <Pagination
-              currentPage={currentPage}
-              pageNum={totalPages}
-              setCurrentPage={setCurrentPage}
-            />
-          </>
-        ) : (
-          <NoneAlertBox>
-            <NoneAlertImg src={noScrapImg} />
-            <NoneAlertTextBox>
-              <NoneAlertText className="title"> 일정플로우가 없습니다!</NoneAlertText>
-              <NoneAlertText>지금 바로 일정플로우를 만들어보세요.</NoneAlertText>
-            </NoneAlertTextBox>
-          </NoneAlertBox>
-        )}
-      </MainSection>
-    </SideNavLayout>
+    <FlowTabLayout
+      selectedPage="watch-flow"
+      isloading={loading}
+      currentPage={currentPage}
+      totalPages={totalPages}
+      handlePaginationClick={setCurrentPage}
+    >
+      <SortControl setOption={setOrder} selectedOption={order} marginright="24px" isFlow={true} />
+      {!loading && datas && datas.length > 0 ? (
+        <>
+          <FlowBox>
+            {datas.map((data) => (
+              <FlowCardD content={data}>
+                <ScrapBtn
+                  flowId={data.id}
+                  isScrap={data.isScraped}
+                  onClick={() => handleScrapBtnClick(data.id)}
+                />
+              </FlowCardD>
+            ))}
+          </FlowBox>
+        </>
+      ) : (
+        <NoneAlertBox>
+          <NoneAlertImg src={noScrapImg} />
+          <NoneAlertTextBox>
+            <NoneAlertText className="title"> 일정플로우가 없습니다!</NoneAlertText>
+            <NoneAlertText>지금 바로 일정플로우를 만들어보세요.</NoneAlertText>
+          </NoneAlertTextBox>
+        </NoneAlertBox>
+      )}
+    </FlowTabLayout>
   );
 }
-
-const MainSection = styled.main`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  align-items: center;
-  padding: 1.88rem 8.38rem 7rem;
-  border-right: 0.5px solid ${({ theme }) => theme.color.grayscale05};
-`;
-
-// 플로우 만들기 (캐릭터 + 버튼)
-const FlowMakeBox = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  margin-right: auto;
-`;
-const FlowMakeCharacter = styled.img`
-  width: 12.47rem;
-  z-index: 2;
-`;
-const FlowMakeBtn = styled.button`
-  position: absolute;
-  left: 10.94rem;
-  width: 32.93rem;
-  height: 6.81rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1;
-  border-radius: 6.25rem;
-  background-color: ${({ theme }) => theme.color.main01};
-  font-size: ${({ theme }) => theme.text.h2.fontSize};
-  font-weight: ${({ theme }) => theme.text.h2.fontWeight};
-  color: ${({ theme }) => theme.color.main05};
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.color.main02};
-    transition: 0.2s;
-  }
-`;
 
 // 플로우 박스 - Grid
 const FlowBox = styled.div`
@@ -217,10 +154,4 @@ const NoneAlertText = styled.span`
     font-size: ${({ theme }) => theme.text.h4.fontSize};
     font-weight: ${({ theme }) => theme.text.h4.fontWeight};
   }
-`;
-
-// 오른쪽 여백
-const PageMarginRight = styled.div`
-  width: 5.7325%;
-  background-color: #f7f8f9;
 `;
