35cddaeb14b98faf0c16a1f75913bf4e0a1db59b
yongmin01 <heart77123@naver.com>
2025-02-14 17:09:11 +0900

    ♻️Refactor: 로그아웃 모달 리팩토링

--- src/components/modal/LogoutModal.jsx
+++ src/components/modal/LogoutModal.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import x from '../../assets/main/closeIcon.svg';
 import * as S from './modal.style';
 import LogoutP from '../../assets/mypage/LogoutImg.svg';
-import { privateAPI } from '../../apis/user';
 import useLoginStore from '../../stores/loginStore';
+import { privateAPI } from '../../apis/user';
 import { useNavigate } from 'react-router-dom';
 
 export default function LogoutModal({ handleModal }) {
   const { logout } = useLoginStore();
+
   const navigate = useNavigate();
   const handleLogout = async () => {
     try {
@@ -26,42 +26,43 @@ export default function LogoutModal({ handleModal }) {
     }
     handleModal(false);
   };
+
   return (
-    <Container>
+    <S.Backdrop>
       <S.Modal>
         <S.Content className="hasimage">
           <S.TitleContainer>
             <S.Title>로그아웃 하시게요?</S.Title>
             <S.SubTitle>더 많은 혜택이 기다리고 있어요.</S.SubTitle>
           </S.TitleContainer>
-          <LogoutImg src={LogoutP} />
+          <LogoutImg src={LogoutP} alt="우는 아브브" />
           <S.BtnContainer className="btn-row">
-            <S.BtnB onClick={handleLogout} className="btn-row">
+            <S.BtnB
+              onClick={handleLogout}
+              className="btn-row"
+              backgroundColor="main05"
+              color="grayscale03"
+              border
+            >
               로그아웃
             </S.BtnB>
-            <S.BtnF onClick={() => handleModal(false)} className="btn-row">
+            <S.BtnF
+              onClick={() => handleModal(false)}
+              className="btn-row"
+              backgroundColor="main02"
+              color="main05"
+            >
               닫기
             </S.BtnF>
           </S.BtnContainer>
         </S.Content>
-        <S.CloseBtn onClick={() => handleModal(false)} src={x} />
       </S.Modal>
-    </Container>
+    </S.Backdrop>
   );
 }
-const Container = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  z-index: 999;
-`;
+
 const LogoutImg = styled.img`
-  width: 200px;
-  height: 210px;
-  margin-bottom: 20px;
+  width: 12.5rem;
+  margin-top: 0.4rem;
+  margin-bottom: 1.69rem;
 `;
--- src/components/modal/modal.style.jsx
+++ src/components/modal/modal.style.jsx
@@ -1,11 +1,23 @@
 import styled from 'styled-components';
+import Button from '../common/button/Button';
 
+const Backdrop = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(70, 76, 82, 0.5);
+  z-index: 999;
+`;
 const Modal = styled.div`
   width: 440px;
   height: 435px;
   display: flex;
   justify-content: center;
-  margin-top: 230px;
   position: relative;
   background-color: #f7f8f9;
   border-radius: 20px;
@@ -28,24 +40,20 @@ const Content = styled.div`
 const TitleContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 8px;
+  gap: 0.5rem;
 `;
 const Title = styled.h1`
-  color: #000;
+  color: ${({ theme }) => theme.color.grayscale01};
+  font-size: ${({ theme }) => theme.text.h3.fontSize};
+  font-weight: ${({ theme }) => theme.text.h3.fontWeight};
   text-align: center;
-  font-family: Pretendard;
-  font-size: 2.16rem;
-  font-style: normal;
-  font-weight: 700;
   line-height: normal;
 `;
 const SubTitle = styled.span`
-  color: #000;
+  color: ${({ theme }) => theme.color.grayscale01};
+  font-size: ${({ theme }) => theme.text.paragraph.fontSize};
+  font-weight: ${({ theme }) => theme.text.paragraph.fontWeight};
   text-align: center;
-  font-family: Pretendard;
-  font-size: 1.25rem;
-  font-style: normal;
-  font-weight: 400;
   line-height: normal;
 `;
 const BtnContainer = styled.div`
@@ -60,41 +68,17 @@ const BtnContainer = styled.div`
     flex-direction: row;
   }
 `;
-const BtnF = styled.button`
-  width: 80%;
-  border-radius: 50px;
-  border: none;
-  background: var(--main-4036-ed, #4036ed);
-  padding: 0.75em;
-  color: var(--main-ffffff, #fff);
-  text-align: center;
-  font-family: Pretendard;
-  font-size: 1.25rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  cursor: pointer;
+const BtnF = styled(Button)`
+  width: 70%;
   &.btn-row {
-    width: auto;
+    width: 8.6rem;
     flex: 1;
   }
 `;
-const BtnB = styled.button`
-  width: 80%;
-  border-radius: 50px;
-  border: 1px solid var(--gray-scale-464-c-52, #464c52);
-  background: var(--main-ffffff, #fff);
-  padding: 0.75em;
-  color: var(--gray-scale-464-c-52, #464c52);
-  text-align: center;
-  font-family: Pretendard;
-  font-size: 1.25rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  cursor: pointer;
+const BtnB = styled(Button)`
+  width: 70%;
   &.btn-row {
-    width: auto;
+    width: 8.6rem;
     flex: 1;
   }
 `;
@@ -105,4 +89,15 @@ const CloseBtn = styled.img`
   cursor: pointer;
 `;
 
-export { Modal, Content, TitleContainer, Title, SubTitle, BtnContainer, BtnF, BtnB, CloseBtn };
+export {
+  Backdrop,
+  Modal,
+  Content,
+  TitleContainer,
+  Title,
+  SubTitle,
+  BtnContainer,
+  BtnF,
+  BtnB,
+  CloseBtn,
+};
