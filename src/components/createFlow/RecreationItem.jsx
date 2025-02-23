fee0a5d3025c222421a6f6bf93172035b6af5dbb
eomgerm <fishbread00@gmail.com>
2025-02-23 15:06:04 +0900

    :sparkles: [Feat]: 플레이 시간 수정 기능 추가

--- src/components/createFlow/RecreationItem.jsx
+++ src/components/createFlow/RecreationItem.jsx
@@ -3,15 +3,26 @@ import circleXIcon from '../../assets/circle_x.svg';
 import KeywordChip from '../common/chip/KeywordChip';
 import styled from 'styled-components';
 import DeleteRecreationConfirmTooltip from './DeleteRecreationConfirmTooltip';
+import AlertMessage from '../common/AlertMessage';
+import { useFormState, useWatch } from 'react-hook-form';
 
 export default function RecreationItem({
   index,
   removeRecreation,
   recreationTitle,
   keywords,
-  playTime,
+  register,
+  control,
 }) {
   const [isDeleteConfirmTooltipOpen, setIsDeleteConfirmTooltipOpen] = useState(false);
+
+  const { playTime } = useWatch({
+    name: `recreations.${index}`,
+    control,
+  });
+
+  const { errors } = useFormState({ control, name: `recreations` });
+  const error = errors.recreations?.[index];
 
   const handleXClick = () => {
     setIsDeleteConfirmTooltipOpen(true);
@@ -53,7 +64,15 @@ export default function RecreationItem({
           ))}
         </KeywordBox>
         <PlayTimeBox>
-          플레이까지 <span>{playTime}분</span>
+          플레이까지
+          <PlayTimeInput
+            placeholder="10"
+            {...register(`recreations.${index}.playTime`, { required: true, min: 1 })}
+          />
+          <span className="minute">분</span>
+          <ErrorWrapper className="playTime">
+            {error?.playTime && <AlertMessage message="플레이 시간을 입력해주세요." />}
+          </ErrorWrapper>
         </PlayTimeBox>
       </RecreationContent>
     </FlowRecreationContainer>
@@ -111,16 +130,44 @@ const Title = styled.div`
 `;
 
 const PlayTimeBox = styled.div`
+  display: flex;
+  align-items: center;
   margin-top: 1rem;
   ${({ theme }) => theme.text.small};
 
-  span {
+  span.minute {
     ${({ theme }) => theme.text.smallBold};
-    margin-left: 1.2rem;
+  }
+`;
+
+const PlayTimeInput = styled.input`
+  margin-left: 1.2rem;
+  font-weight: 700;
+  border: none;
+  ${({ theme }) => theme.text.smallBold};
+  width: 1.1rem;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.color.grayscale04};
   }
 `;
 
 const DeleteButton = styled.button`
   padding: 0;
   height: 1.25rem;
+`;
+
+const ErrorWrapper = styled.div`
+  &.title {
+    margin-left: 3.3rem;
+    margin-top: -1rem;
+  }
+
+  &.keywords {
+    margin-top: -0.5rem;
+  }
+
+  &.playTime {
+    margin-left: 0.5rem;
+  }
 `;
