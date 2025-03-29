b25532ad1ae1efb1822bd330a1c29ea3118ff4cc
eomgerm <fishbread00@gmail.com>
2025-03-30 01:04:09 +0900

    :construction_worker: [Chore]: prerender 임시 적용

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,26 @@ import * as path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
+import prerender from '@prerenderer/rollup-plugin';
 
 export default defineConfig({
-  plugins: [react(), svgrPlugin()],
+  plugins: [
+    react(),
+    svgrPlugin(),
+    prerender({
+      routes: ['/', '/search/list'],
+      renderer: '@prerenderer/renderer-puppeteer',
+      server: {
+        host: 'localhost',
+        port: 3000,
+      },
+      postProcess(renderedRoute) {
+        renderedRoute.html = renderedRoute.html
+          .replace(/http:/i, 'https:')
+          .replace(/(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i, 'https://avab.site/');
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
